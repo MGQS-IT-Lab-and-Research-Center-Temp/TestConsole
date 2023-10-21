0b383990a7991a84eb5efeb67d31aa36c6cde1e5
absyinka <absyinka@gmail.com>
2023-10-21 14:46:47 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+string[] cars = new string[] { "Lamborghini", "Rolls Royce", "Bugatti Veronni", "Ferrari", "Tesla" };
+
+try
+{
+    List<string> names = new() { "Ahmad Raza", "Abdul Tawwab", "Hasbiyallah" };
+
+    List<int> numbers = new() { 15 };
+
+    if (names.Count >= 3)
+    {
+        for (int i = 0; i < names.Count; i++)
+        {
+            Console.WriteLine(names[i] + " is " + numbers[i] + " years old");
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
